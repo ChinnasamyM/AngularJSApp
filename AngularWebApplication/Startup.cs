6b9ce86f87e9cfe308b33ad3a1b22f915f89039d
ChinnasamyM <chinnasamym@live.com>
2015-11-18 13:18:32 +0530

    AngularJS app Initial commit with lib's config

---
+++ AngularWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularWebApplication.Startup))]
+namespace AngularWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
